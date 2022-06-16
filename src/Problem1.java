3082509aa1ee8aedecd3a54caeae5e16b847b10c
Tariq <tarsabree@gmail.com>
2022-06-16 09:17:25 -0500

    update

--- src/Problem1.java
+++ src/Problem1.java
@@ -2,41 +2,45 @@ import java.util.Scanner;
 
 public class Problem1 {
     public static void main(String[] args) {
-
+        //Create scanner object
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter a");
+
+        System.out.print("Enter a: ");
         int a = input.nextInt();
-        System.out.println("Enter b");
+        System.out.print("Enter b: ");
         int b = input.nextInt();
-        System.out.println("Enter c");
+        System.out.print("Enter c: ");
         int c = input.nextInt();
-        System.out.println("Enter d");
+        System.out.print("Enter d: ");
         int d = input.nextInt();
 
-sum(a,b,c,d);
-multiple(a,b,c);
-division(a,b);
-subtraction(a,b);
+        // Call methods
+        sum(a,b,c,d);
+        multiple(a,b,d);
+        division(b,d);
+        subtraction(b,c);
     }
-    public static int sum(int a ,int b, int c, int d)
+    // Define sum, multiple, division and subtraction methods
+    public static void sum(int a ,int b, int c, int d)
     {
         int results = a + b + c+ d;
-        System.out.println(a+"+"+b+"+"+c +"+"+d+"="+results);
-        return results;
+        System.out.println(a + " + " + b + " + " + c + " + " + d + " = " + results);
     }
-public static int multiple( int a, int b, int c)
-        {int results = a * b * c;
-            System.out.println(a+"*"+b+"*"+c +"="+results);
-        return results;
-
-}public static double division( int a, int b)
-    {double results = (double) a / b;
-        System.out.println(a+"/"+b +"="+results);
-        return results;}
-public static int subtraction ( int a, int b)
-{int results =  b-a;
-    System.out.println(b+"-"+a +"="+results);
-    return results;}
+    public static void multiple(int a, int b, int d)
+    {
+        int results = a * b * d;
+        System.out.println(a + "*" + b + "*" + d + "=" + results);
+    }
+    public static void division(int b, int d)
+    {
+        double results = (double) d / b;
+        System.out.println(d + "/" + b + "=" + results);
+    }
+    public static void subtraction(int b, int c)
+    {
+        int results =  b-c;
+        System.out.println(b + "-" + c + "=" + results);
+    }
 }
 
 
